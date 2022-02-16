4d5555f4dccd074f1ffd8fb40d6b07f9dfd7000a
Dani <danihazler@gmail.com>
2022-02-16 16:30:58 +0000

    Fix basket icon hover effect

--- components/icons/shop-basket.js
+++ components/icons/shop-basket.js
@@ -6,11 +6,7 @@ export const ShopBasketIcon = () => {
       width="50"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="m27 22v-6h-3v6h-11l5 12h15l5-12z"
-        fill="#1c1c20"
-        fillRule="evenodd"
-      />
+      <path d="m27 22v-6h-3v6h-11l5 12h15l5-12z" fillRule="evenodd" />
     </svg>
   );
 };
